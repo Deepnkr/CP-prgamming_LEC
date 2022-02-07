c56bfb2f665e0be0e6a0c7c84c5d0f7fab0904bc
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-02-07 09:17:57 +0545

    for loop 1

---
+++ Chapter3_Cbasic/loops/for1_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for(i=9; i>=1;)
+    {
+        printf("*\n");
+        i--;
+    }
+}
---
+++ Chapter3_Cbasic/loops/for2_loop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int i=0, j=0;
+    for(i; i<5;)
+    {
+        printf("%d %d\n",i,j);
+        i++;j++;
+    }
+}
