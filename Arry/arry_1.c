56a66cec89ba6ef31e13d2546a78c1c0393fbeb8
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-02-22 06:40:09 +0545

    arry practic

---
+++ Arry/arry_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int marks[5];
+    marks[0] = 6;
+    printf("marks of student 1 = %d\n",marks[0]);
+    marks[0] = 66;
+    marks[1] = 55;
+    marks[2] = 44;
+    marks[3] = 33;
+    marks[4] = 22;
+    printf("marks of student 1 = %d\n",marks[1]);
+}
