3322aa78431e8df1efb4b7138e7bb6a2a7b9aa05
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-03-02 19:44:12 +0545

    patterns

--- Extra/loop2.c
+++ Extra/loop2.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int i,j;
-    for ( i = 5; i >=1; i--)
+    int i,j,k =0;
+    for ( i = 1; i <=5; i++)
     {
-        for(j=5;j>=i;j--)
+        for(j=i;j<=5;j++)
         {
             printf("%d ",j);
+        
         }
+        
         printf("\n");
+        printf("0");
     }
 }
