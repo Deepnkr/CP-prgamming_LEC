ff29812123689537281e7c16f131bcc352e328e8
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-03-07 09:15:30 +0545

    to find the sum of num dividual by 7 but not by 5

---
+++ Self_practic_from_beginning/arry_part1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int arry[5],i,sum=0;
+    for( i=0; i<5; i++)
+    {
+        printf("enter arry[%d]:",i);
+        scanf("%d",&arry[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        if(arry[i]%7==0&&arry[i]%5!=0)
+        {
+            sum = sum+arry[i];
+            printf("the numbers are = %d\n",arry[i]);
+        }
+
+    }
+    printf("sum of num is:%d",sum);
+    
+}
