d0c1a62a2c026ea8cff7e61bca00e866f501ecde
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-03-24 10:05:08 +0545

    swaping value of 2 pointer value

---
+++ Pointer/tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int*a,int*b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int A=5,B=10;
+    swap(&A, &B);
+    printf("value of A = %d\n",A);
+    printf("value of B = %d",B);
+}
