3b86a523f5e4d0978408f7af0b46a5c5256d62d4
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-03-28 19:38:19 +0545

    Basic Arithmatic Operations

---
+++ Self_practic_from_beginning/Basic_operator's/Arithmetic_Operation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    a = 13;
+    b = 15.88;
+    printf("a = %d\nb = %f\n",a,b);
+    printf("a + b = %f\n",a+b);
+    printf("a - b = %f\n",a-b);
+    printf("a * b = %f\n",a*b);
+    printf("a / b = %f\n",a/b);
+}
