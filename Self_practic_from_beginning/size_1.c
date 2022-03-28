9bde304696b9674c2cbb4ff060fb3aafcdefc276
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-03-28 11:03:06 +0545

    size of variable type

---
+++ Self_practic_from_beginning/size_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+char main()
+{
+    printf("hello world\n");
+    printf("size of char is %d\n",sizeof(char));
+    printf("size of int is %d\n",sizeof(int));
+    printf("size of float is %d\n",sizeof(float));
+    return 0;
+}
