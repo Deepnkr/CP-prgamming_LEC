1e317d6688a77672c26f18c53bd85a9b442afcaa
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-04-28 07:57:29 +0545

    Question no 1 ans

---
+++ Assignment_2022_01_26/Qestion_no_1.c
@@ -0,0 +1,13 @@
+// Wap to calculate the volm of pool by using function and already given value.
+
+#include<stdio.h>
+int volume_of_pool(int l,int w,int d)
+{
+    int c;
+    c = l*w*d;
+    return c;
+}
+int main()
+{
+    printf("The volumn of pool is = %d",volume_of_pool(25,10,6));
+}
