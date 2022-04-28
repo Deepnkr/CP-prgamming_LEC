0603e0d35cb70e2529261f46677dbe3a5b685e53
Deepankar Shakya <shakyadeepankar4@gmail.com>
2022-04-28 08:12:33 +0545

    Question no 2 ans

---
+++ Assignment_2022_01_26/Question_no_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int question_2(float a, float b)
+{
+    int c,d;
+    float e;
+    printf("Sum of numbers are\n");
+    c = a+b;
+    printf("%d\n",c);
+
+    printf("product of numbers is\n");
+    d = a*b;
+    printf("%d\n",d);
+
+    printf("quotient of numbers is\n");
+    e = a/b;
+    printf("%f",e);
+}
+int main()
+{
+    question_2(22,7);
+}
